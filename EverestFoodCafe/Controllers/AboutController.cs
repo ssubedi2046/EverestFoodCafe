3b64c1f1c2b0bb0f759ed4cfe358ca947344b6b7
Sushil <ssushil2016@gmail.com>
2019-10-27 12:00:21 -0500

    changed controller method body

--- EverestFoodCafe/Controllers/AboutController.cs
+++ EverestFoodCafe/Controllers/AboutController.cs
@@ -9,9 +9,6 @@ namespace EverestFoodCafe.Controllers
     public class AboutController : Controller
     {
         // GET: About this is changes 1
-        public ActionResult About()
-        {
-            return View();
-        }
+        public ActionResult About() => View();
     }
 }
